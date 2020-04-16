a5553d8a7496334830575a4e4ff7765889c8a7eb
FoushWare <mr.fouad992@gmail.com>
2020-04-17 00:11:34 +0200

    return channel from channel mutation

--- formatErrors.js
+++ formatErrors.js
@@ -1,11 +1,11 @@
 import _ from 'lodash';
 
 export default (e, models) => {
-  if (e instanceof models.sequelize.ValidationError) {
+  //if (e instanceof models.sequelize.ValidationError) {
     //  _.pick({a: 1, b: 2}, 'a') => {a: 1}
     return e.errors.map(x => _.pick(x, ['path', 'message']));
-  }
-  return [{ path: 'name', message: 'something went wrong' }];
+ // }
+  //return [{ path: 'name', message: 'something went wrong' }];
 };
 
 
--- schema/Channel.js
+++ schema/Channel.js
@@ -6,7 +6,12 @@ export default `
     messages: [Message!]!
     users: [User!]!
   }
+  type ChannelResponse {
+    ok: Boolean!
+    channel: Channel
+    errors: [Error!]
+  }
   type Mutation {
-    createChannel(teamId: Int!, name: String!, public: Boolean=false): Boolean!
+    createChannel(teamId: Int!, name: String!, public: Boolean=false): ChannelResponse!
   }
 `;
